ad814898c86a12abb9d3c7fb9d8328794ae9d629
Josue Patricio <josretamozovar@gmail.com>
2025-01-17 08:40:19 -0500

    Update restaurant listing to use Link component for navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import api from "@/api";
 import RestaurantCard from "@/components/custom/restaurant-card";
+import Link from "next/link";
 
 export default async function Home() {
   const restaurants = await api.list();
@@ -7,13 +8,15 @@ export default async function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold text-center">Restaurantes</h1>
-        <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+
+        <section className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
           {restaurants.map((restaurant) => (
-            <li key={restaurant.id}>
+            <Link key={restaurant.name} href={restaurant.id}>
               <RestaurantCard {...restaurant} />
-            </li>
+            </Link>
           ))}
-        </ul>
+        </section>
+
       </main>
     </div>
   );
