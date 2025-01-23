11ded44b7ae996b1c0f2784f462dc57ec388b2ff
Josue Patricio <josretamozovar@gmail.com>
2025-01-23 17:00:53 -0500

    se agrego opengraph

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -24,6 +24,23 @@ export async function generateMetadata({
   return {
     title: name,
     description,
+    openGraph: {
+      title: name,
+      description,
+      type: 'website',
+      images: [{
+        url: `/restaurant/${id}/opengraph-image`,
+        width: 1200,
+        height: 630,
+        alt: name
+      }]
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: name,
+      description,
+      images: [`/restaurant/${id}/opengraph-image`],
+    }
   };
 }
 
